4c31f3e6231ac5b3b218a58baafe2f2b15834b3b
Kevyn Herrera <kherrera1738@berkeley.edu>
2021-01-07 20:57:19 -0800

    added full title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title pm a per-page basis
+  def full_title(page_title= '')
+      base_title = "Ruby on Rails"
+      if page_title.empty?
+        return base_title
+      else
+        return page_title
+      end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", "Ruby on Rails"
   end
 
   test "should get help" do
